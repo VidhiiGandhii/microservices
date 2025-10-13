5437c7ad84e6951d270b08fb561ed73bb5a0a381
VIDHI GANDHI <vidhi.gandhi.20@gmail.com>
2025-10-13 16:02:23 +0530

    new changes

--- configserver/src/main/java/com/micro/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/micro/configserver/ConfigserverApplication.java
@@ -7,9 +7,20 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigserverApplication {
-
+	
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigserverApplication.class, args);
+
+		String mode=System.getenv("CONFIG_MODE");
+		SpringApplication app=new SpringApplication(ConfigserverApplication.class);
+		
+	if("native".equalsIgnoreCase(mode)) {
+		app.setAdditionalProfiles("native");
+	}
+	else {
+		app.setAdditionalProfiles("git");
+	}
+	app.run(args);
+		
 	}
 
 }
